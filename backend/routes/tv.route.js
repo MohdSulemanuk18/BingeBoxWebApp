73995285bbe268b49129c00e3c483514c1d735f9
Mohd Suleman <sulemangit@gmail.com>
2025-06-30 15:10:02 +0530

    updated successfully

--- backend/routes/tv.route.js
+++ backend/routes/tv.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {
-	getSimilarTvs,
-	getTrendingTv,
-	getTvDetails,
-	getTvsByCategory,
-	getTvTrailers,
+  getSimilarTvs,
+  getTrendingTv,
+  getTvDetails,
+  getTvsByCategory,
+  getTvTrailers
 } from "../controllers/tv.controller.js";
 
 const router = express.Router();
@@ -13,6 +13,6 @@ router.get("/trending", getTrendingTv);
 router.get("/:id/trailers", getTvTrailers);
 router.get("/:id/details", getTvDetails);
 router.get("/:id/similar", getSimilarTvs);
-router.get("/category/:category", getTvsByCategory);
+router.get("/:category", getTvsByCategory);
 
 export default router;
